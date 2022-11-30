47fb843bf2842acadff11359dbadb36060685445
KTMMMM <110892552+KTMMMM@users.noreply.github.com>
2022-11-30 17:31:38 +0900

    Update relationship.py

--- analyze/relationship.py
+++ analyze/relationship.py
@@ -11,14 +11,13 @@ def combine_2rd_columns(col_1, col_2):
     return result
 
 def get_user_data(user_data):
-
     json_data = user_data.dict()
     user_data_list = json_data['coorperate']
-
     user_data_df = pd.DataFrame(user_data_list)
     user_data_df.sort_values('time')
     user_data_df["coord"] = user_data_df.apply(lambda x: combine_2rd_columns(x['x'], x['z']), axis=1)
     user_data_df = user_data_df.drop(['x', 'z'], axis=1)
+    user_data_df = user_data_df.drop_duplicates(['id','time'])
     users = user_data_df.id.unique()
     log_Users = []
     for i in users:
@@ -27,11 +26,9 @@ def get_user_data(user_data):
         globals()['userids_{}'.format(i)] = globals()['userid_{}'.format(i)]["coord"]
         time_Index = list(globals()['userid_{}'.format(i)]["time"])
         globals()['userid_{}'.format(i)] = pd.Series(globals()['userids_{}'.format(i)].values,index=time_Index).rename('{}'.format(i))
-        log_Users.append('{}'.format(i))
-        
+        log_Users.append('{}'.format(i)) 
     log_Table = pd.concat([globals()['userid_{}'.format(i)] for i in log_Users], axis=1)
     log_Table.fillna('Offline',inplace=True)
-    # print(log_Table)
     return log_Table
 
 def user_relationship(user_data):
@@ -74,16 +71,34 @@ def user_relationship(user_data):
             if nearly_user_distance != 999:     
                 friendly_df.loc[time_recorded,base_user_name]=nearly_user_name
                 secondly_df.loc[time_recorded,base_user_name]=second_user_name
+    print(friendly_df)
+    print(secondly_df)
     location_df = user_location(user_data)
     for id in user_columns:
         friendly_list = friendly_df[id].values
         friendly_list = [item for item in friendly_list if not(pd.isnull(item)) == True]
         secondly_list = secondly_df[id].values
         secondly_list = [item for item in secondly_list if not(pd.isnull(item)) == True]
-        most_friendly = max(friendly_list, key = friendly_list.count)
-        second_friendly = max(secondly_list, key = secondly_list.count)
-        spend_time_with_most = friendly_df[id].value_counts().max() * 10
-        spend_time_with_second = secondly_df[id].value_counts().max() * 10
+        if len(friendly_list)!= 0:
+            most_friendly = max(friendly_list, key = friendly_list.count)
+            spend_time_with_most = friendly_df[id].value_counts().max() * 10
+            if most_friendly == "":
+                most_friendly="None"
+                spend_time_with_most=0
+        else:
+            most_friendly = "None"
+            spend_time_with_most = 0
+        
+        if len(secondly_list) != 0:
+            second_friendly = max(secondly_list, key = secondly_list.count)
+            spend_time_with_second = secondly_df[id].value_counts().max() * 10
+            if second_friendly=="":
+                second_friendly="None"
+                spend_time_with_second=0
+        else:
+            second_friendly = "None"
+            spend_time_with_second = 0
+            
         place_list = location_df[id].values
         place_list = [item for item in place_list if not(pd.isnull(item)) == True]
         most_place = max(place_list, key = place_list.count)  #가장 많이 논 곳
